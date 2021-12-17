7253cccd8203c9021171a08b951c062872aa182b
Kylie <kylie@comets.kr>
2021-12-17 17:38:43 +0900

    modify: chevron function

--- src/public/js/common/chevron.js
+++ src/public/js/common/chevron.js
@@ -3,20 +3,21 @@ const chevron_down = document.getElementById('main__list-chevron-down')
 const main_list = document.getElementById("main_list")
 const HIDDEN_CLASSNAME = "hidden"
 
-function handleChevronUp(){
-    chevron_up.classList.add(HIDDEN_CLASSNAME)
-    chevron_down.classList.remove(HIDDEN_CLASSNAME)
-    main_list.classList.remove(HIDDEN_CLASSNAME)
-}
-
 function handleChevronDown(){
     chevron_down.classList.add(HIDDEN_CLASSNAME)
     main_list.classList.add(HIDDEN_CLASSNAME)
     chevron_up.classList.remove(HIDDEN_CLASSNAME)
 }
 
+function handleChevronUp(){
+    chevron_up.classList.add(HIDDEN_CLASSNAME)
+    chevron_down.classList.remove(HIDDEN_CLASSNAME)
+    main_list.classList.remove(HIDDEN_CLASSNAME)
+}
+
 function toggleChevron(){
-    chevron_up.addEventListener("click", handleChevronUp)
     chevron_down.addEventListener("click", handleChevronDown)
+    chevron_up.addEventListener("click", handleChevronUp)
+
 }
 toggleChevron()
