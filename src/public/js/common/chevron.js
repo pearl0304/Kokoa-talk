8520f6852d9846049a59efac1a7acbf4af33c7e7
kylie <wlswn4630@gmail.com>
2021-12-18 22:08:52 +0900

    feat: toggle in friends page

--- src/public/js/common/chevron.js
+++ src/public/js/common/chevron.js
@@ -1,6 +1,9 @@
 const chevron_up = document.getElementById('main__list-chevron-up')
 const chevron_down = document.getElementById('main__list-chevron-down')
 const main_list = document.getElementById("main_list")
+const friends_chevron_up = document.getElementById('friends__list-chevron-up')
+const friends_chevron_down = document.getElementById('friends__list-chevron-down')
+const friends_list = document.getElementById("friends_list")
 const HIDDEN_CLASSNAME = "hidden"
 
 function handleChevronDown(){
@@ -15,9 +18,25 @@ function handleChevronUp(){
     main_list.classList.remove(HIDDEN_CLASSNAME)
 }
 
+function handleFriendsChevronDown(){
+    friends_chevron_down.classList.add(HIDDEN_CLASSNAME)
+    friends_list.classList.add(HIDDEN_CLASSNAME)
+    friends_chevron_up.classList.remove(HIDDEN_CLASSNAME)
+}
+
+function handleFriendsChevronUp(){
+    friends_chevron_up.classList.add(HIDDEN_CLASSNAME)
+    friends_chevron_down.classList.remove(HIDDEN_CLASSNAME)
+    friends_list.classList.remove(HIDDEN_CLASSNAME)
+}
+
+
+
 function toggleChevron(){
     chevron_down.addEventListener("click", handleChevronDown)
     chevron_up.addEventListener("click", handleChevronUp)
+    friends_chevron_down.addEventListener("click", handleFriendsChevronDown)
+    friends_chevron_up.addEventListener("click", handleFriendsChevronUp)
 
 }
 toggleChevron()
