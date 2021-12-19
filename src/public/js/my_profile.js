5a6966efecca4034c1b3fd4c2f4dc26a5dda4b8e
kylie <wlswn4630@gmail.com>
2021-12-19 22:51:46 +0900

    modify: hidden -> hidden_classname

--- src/public/js/my_profile.js
+++ src/public/js/my_profile.js
@@ -1,11 +1,11 @@
 const editBtn = document.getElementById('profile__edit-btn')
 const profileImgBtn = document.getElementById('profile-img-btn')
 const statusMessage = document.getElementById('status-message')
-const HIDDEN ='hidden'
+const HIDDEN_CLASSNAME ='hidden'
 
 function showEdit(){
-    profileImgBtn.classList.toggle(HIDDEN)
-    statusMessage.classList.toggle(HIDDEN)
+    profileImgBtn.classList.toggle(HIDDEN_CLASSNAME)
+    statusMessage.classList.toggle(HIDDEN_CLASSNAME)
 }
 
 function clickEdit(){
