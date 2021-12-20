4347fac64779f012d89e9dea146916fea34679eb
Kylie <kylie@comets.kr>
2021-12-20 17:17:11 +0900

    setting: chat-room

--- controllers/chat_ctrl.js
+++ controllers/chat_ctrl.js
@@ -1,7 +1,32 @@
+import { ObjectId } from 'mongodb'
+import {findUserDataById} from '../models/user.js'
 export const chatController = {
     getChatListPage : async(req,res)=>{
         try{
             res.render('chat-list')
+        }catch(e){
+            console.error(e)
+        }
+    },
+    getChatRoom : async(req,res)=>{
+        try{
+            // friend data
+            const params = req.params['id']
+            const id =  params.slice(1,params.length)
+            const friend_id = ObjectId(id)
+            const friend = await findUserDataById(friend_id)
+
+
+            // user data
+            
+            const tokenData = (req.body.userData['_id'])
+            const user_id = ObjectId(tokenData)
+            const user = await findUserDataById(user_id)
+
+          
+
+            res.render('chat-room')
+
         }catch(e){
             console.error(e)
         }
--- routers/chat_router.js
+++ routers/chat_router.js
@@ -1,7 +1,14 @@
 import { Router } from "express"
 const chatRouter = Router()
 import {chatController} from "../controllers/chat_ctrl.js"
-
+import { mainController } from "../controllers/main_ctrl.js";
 
 chatRouter.get('/list',chatController.getChatListPage)
+chatRouter.get('/room/:id',mainController.getTokenData,chatController.getChatRoom)
+
+
+
+
+
+
 export default chatRouter
