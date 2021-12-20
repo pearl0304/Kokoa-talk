c07963abb5a2c9c76d2d9c4166b21fd9f1f4d05c
Kylie <kylie@comets.kr>
2021-12-20 10:27:22 +0900

    modify: Make input element for user nickname changing

--- src/public/js/my_profile.js
+++ src/public/js/my_profile.js
@@ -1,11 +1,17 @@
 const editBtn = document.getElementById('profile__edit-btn')
-const profileImgBtn = document.getElementById('profile-img-btn')
-const statusMessage = document.getElementById('status-message')
+const originUserNick = document.getElementById('origin-userNick')
+const profileImgBtn = document.querySelector('.profile-profileImg #profile-img-btn')
+const profileEdit = document.getElementById('profile-edit')
+const profileEditBtn = document.getElementById('profile-edit-btn')
+
 const HIDDEN_CLASSNAME ='hidden'
 
 function showEdit(){
+    originUserNick.classList.toggle(HIDDEN_CLASSNAME)
     profileImgBtn.classList.toggle(HIDDEN_CLASSNAME)
-    statusMessage.classList.toggle(HIDDEN_CLASSNAME)
+    profileEdit.classList.toggle(HIDDEN_CLASSNAME)
+    profileEditBtn.classList.toggle(HIDDEN_CLASSNAME)
+
 }
 
 function clickEdit(){
