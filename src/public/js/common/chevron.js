8c0202261eda92f1f6c537c4236c31009d7ff3b1
Kylie <kylie@comets.kr>
2021-12-20 16:10:03 +0900

    Refactoring: chevron.js

--- src/public/js/common/chevron.js
+++ src/public/js/common/chevron.js
@@ -3,40 +3,26 @@ const chevron_down = document.getElementById('main__list-chevron-down')
 const main_list = document.getElementById("main_list")
 const friends_chevron_up = document.getElementById('friends__list-chevron-up')
 const friends_chevron_down = document.getElementById('friends__list-chevron-down')
-const friends_list = document.getElementById("friends_list")
+const friends_list_wrap = document.getElementById("friends_list_wrap")
 const HIDDEN_CLASSNAME = "hidden"
 
-function handleChevronDown(){
-    chevron_down.classList.add(HIDDEN_CLASSNAME)
-    main_list.classList.add(HIDDEN_CLASSNAME)
-    chevron_up.classList.remove(HIDDEN_CLASSNAME)
+function handleChevron(){
+    chevron_up.classList.toggle(HIDDEN_CLASSNAME)
+    chevron_down.classList.toggle(HIDDEN_CLASSNAME)
+    main_list.classList.toggle(HIDDEN_CLASSNAME)
 }
 
-function handleChevronUp(){
-    chevron_up.classList.add(HIDDEN_CLASSNAME)
-    chevron_down.classList.remove(HIDDEN_CLASSNAME)
-    main_list.classList.remove(HIDDEN_CLASSNAME)
+function handleFriendListChevron(){
+    friends_chevron_up.classList.toggle(HIDDEN_CLASSNAME)
+    friends_chevron_down.classList.toggle(HIDDEN_CLASSNAME)
+    friends_list_wrap.classList.toggle(HIDDEN_CLASSNAME)
 }
-
-function handleFriendsChevronDown(){
-    friends_chevron_down.classList.add(HIDDEN_CLASSNAME)
-    friends_list.classList.add(HIDDEN_CLASSNAME)
-    friends_chevron_up.classList.remove(HIDDEN_CLASSNAME)
-}
-
-function handleFriendsChevronUp(){
-    friends_chevron_up.classList.add(HIDDEN_CLASSNAME)
-    friends_chevron_down.classList.remove(HIDDEN_CLASSNAME)
-    friends_list.classList.remove(HIDDEN_CLASSNAME)
-}
-
-
 
 function toggleChevron(){
-    chevron_down.addEventListener("click", handleChevronDown)
-    chevron_up.addEventListener("click", handleChevronUp)
-    friends_chevron_down.addEventListener("click", handleFriendsChevronDown)
-    friends_chevron_up.addEventListener("click", handleFriendsChevronUp)
+    chevron_down.addEventListener("click",handleChevron)
+    chevron_up.addEventListener("click",handleChevron)
+    friends_chevron_down.addEventListener("click",handleFriendListChevron)
+    friends_chevron_up.addEventListener("click",handleFriendListChevron)
 
 }
 toggleChevron()
