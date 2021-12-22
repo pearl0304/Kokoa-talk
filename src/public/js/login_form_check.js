f25fbde180e7444a263e909902023addacfa470b
kylie <wlswn4630@gmail.com>
2021-12-22 21:38:58 +0900

    tmp: fixing validation

---
+++ src/public/js/login_form_check.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('login-form')
+const email = document.getElementsByName('userEmail')
+const userPw = document.getElementsByName('userPw')
+
+function checkInputLength(){
+    
+}
+
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+
+})
